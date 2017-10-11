360057e12350200b6185c04aba2a6bdc2b3b3518
Lucas Santos <lucas1295santos@gmail.com>
2017-10-11 14:36:06 -0300

    Initial

---
+++ PieChartPod/PieChartData.swift
@@ -0,0 +1,15 @@
+//
+//  PieChartData.swift
+//  superSavings
+//
+//  Created by Lucas Santos on 11/10/17.
+//  Copyright © 2017 lucasSantos. All rights reserved.
+//
+
+import UIKit
+
+struct PieChartData{
+    var color: UIColor
+    var percentage: Double
+    var name: String
+}
---
+++ Pods/Target Support Files/Pods-PieChartPod/Pods-PieChartPod-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PieChartPod : NSObject
+@end
+@implementation PodsDummy_Pods_PieChartPod
+@end
